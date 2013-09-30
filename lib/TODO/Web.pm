debaa433178333b3d2461c285f32f7cef8788673
crmar <hipims@gmail.com>
2013-09-30 17:07:01 +0900

    Modification of the display table

--- lib/TODO/Web.pm
+++ lib/TODO/Web.pm
@@ -42,7 +42,7 @@ post '/input' => sub {
 	my $td = $c->req->parameters_raw->{td};
 	my $dl = $c->req->parameters_raw->{dl};
 	my $cat = $c->req->parameters_raw->{cat};
-	my $dbh = $self->dbh([$self]);
+	my $dbh = $self->dbh;
 	my $sth = $dbh->prepare("insert into todo (todo, date, cid) values ('$td', '$dl', $cat)");
 	$sth->execute;
 	$sth->finish;
@@ -53,7 +53,7 @@ $c->redirect('/');
 
 get '/' => [qw/set_title/] => sub {
 	my ( $self, $c ) = @_;
-	my $dbh = $self->dbh([$self]);
+	my $dbh = $self->dbh;
 	my $sth = $dbh->prepare("select t.No, t.todo, t.date, c.cname from todo as t inner join category as c on t.cid = c.cid 
 	order by t.date");
 	$sth->execute;
@@ -74,7 +74,7 @@ $c->render('index.tx', { greeting => "Hello", rows => $row });
 get '/delete' => [qw/set_title/] => sub {
 	my ( $self, $c ) = @_;
 	my $id = $c->req->parameters_raw->{id};
-	my $dbh = $self->dbh([$self]);
+	my $dbh = $self->dbh;
 	my $sth = $dbh->prepare("delete from todo where No=$id");
 	$sth->execute;
 	$sth->finish;
@@ -89,7 +89,7 @@ post '/change' => sub {
 	my $changetd = $c->req->parameters_raw->{changetd};
 	my $changedl = $c->req->parameters_raw->{changedl};
 	my $id = $c->req->parameters_raw->{id};
-	my $dbh = $self->dbh([$self]);
+	my $dbh = $self->dbh;
 	my $sth = $dbh->prepare("update todo set todo='$changetd', date='$changedl' where No=$id");
 	$sth->execute;
 	$sth->finish;
@@ -97,7 +97,19 @@ post '/change' => sub {
 $c->redirect('/');
 };
 
+post '/search' => sub {
+	my ( $self, $c ) = @_;
+	my $cat = $c->req->parameters_raw->{cat};
+	my $dbh = $self->dbh;
+	my $sth = $dbh->prepare("select t.No, t.todo, t.date, c.cname from todo as t inner join category as c on t.cid = c.cid 
+	 where t.cid = $cat order by t.date");
+	$sth->execute;
+	my $row = $sth->fetchall_arrayref();
+	$sth->finish;
+	$dbh->disconnect;
+$c->render('index.tx', { rows => $row });
 
+};
 
 
 
